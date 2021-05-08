9df875c066367c4c1c0b2e197de15f0c2ed0d2b3
treyturley <81778542+treyturley@users.noreply.github.com>
2021-05-08 17:52:46 -0500

    Implement FSE requests and logging for those requests

---
+++ FSEJobFinder-ConsoleApp/FSEDataFeed/FSEDataRequest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Net;
+
+namespace FSEDataFeed
+{
+    class FSEDataRequest
+    {
+        private DateTime timeStamp;
+        private string url;
+        private HttpWebRequest request;
+        private FSEDataRequestType requestType;
+
+        /// <summary>
+        /// Used to create a new FSEDataRequest with the timestamp set to the current time.
+        /// </summary>
+        /// <param name="requestType">the type of the FSE Data Request</param>
+        /// <param name="url">The URL string of the request</param>
+        public FSEDataRequest(FSEDataRequestType requestType, string url)
+        {
+            this.url = url;
+            request = (HttpWebRequest)WebRequest.Create(url);
+            timeStamp = DateTime.Now;
+            this.requestType = requestType;
+        }
+
+
+        public FSEDataRequest(FSEDataRequestType requestType, string url, DateTime timeStamp)
+        {
+            this.requestType = requestType;
+            this.url = url;
+            request = (HttpWebRequest)WebRequest.Create(url);
+            this.timeStamp = timeStamp;
+
+        }
+
+        public FSEDataRequest(string requestObjAsString)
+        {
+            //split it into parts
+            string[] objParts = requestObjAsString.Split(',');
+
+            //TODO: some validatio here could help incase the file get modifed outside of the program
+
+            requestType = (FSEDataRequestType)Enum.Parse(typeof(FSEDataRequestType), objParts[0]);
+            url = objParts[1];
+            request = (HttpWebRequest)WebRequest.Create(url);
+            timeStamp = DateTime.Parse(objParts[2]);
+        }
+
+        public HttpWebResponse GetResponse()
+        {
+            return (HttpWebResponse)request.GetResponse();
+        }
+
+        public string GetURL()
+        {
+            return url;
+        }
+
+        public DateTime GetTimestamp()
+        {
+            return timeStamp;
+        }
+
+        public FSEDataRequestType GetRequestType()
+        {
+            return requestType;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+
+            result += requestType.ToString() + ",";
+            result += url + ",";
+            result += timeStamp.ToString();
+
+            return result;
+        }
+
+    }
+
+    public enum FSEDataRequestType
+    {
+        Aircraft_Configs,
+        Aircraft_Aliases,
+        Aircraft_For_Sale,
+        Aircraft_By_MakeModel,
+        Aircraft_By_Owner_Name,
+        Aircraft_By_Registration,
+        Aircraft_By_Id,
+        Aircraft_By_Key,
+        Assignments_By_Key,
+        Commodities_By_Key,
+        Facilities_By_Key,
+        FBOs_By_Key,
+        FBOs_For_Sale,
+        FBO_Monthly_Summary_by_ICAO,
+        Flight_Logs_By_Key_Month_Year,
+        Flight_Logs_By_Reg_Month_Year,
+        Flight_Logs_By_serialnumber_Month_Year,
+        Flight_Logs_By_Key_From_Id, //limted to 500
+        Flight_Logs_By_Key_From_Id_for_ALL_group_aircraft, //limted to 500
+        Flight_Logs_By_Reg_From_Id, //limted to 500
+        Flight_Logs_By_serialnumber_From_Id, //limted to 500
+        Group_Members,
+        ICAO_Listing_of_Aircraft,
+        ICAO_Listing_of_FBOs,
+        ICAO_Jobs_To,
+        ICAO_Jobs_From,
+        Payments_By_Month_Year,
+        Payments_From_Id_, //limted to 500
+        Statistics_By_Key,
+    }
+}
---
+++ FSEJobFinder-ConsoleApp/FSEDataFeed/FSEDataRequestTracker.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FSEDataFeed
+{
+    //"Timing: The Access Key can hit the server 10 times within any rolling 60 second window, including multiple simultaneous hits. 
+    // Read Access Keys are also limited to 40 hits within a 6 hour rolling window."
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class FSEDataRequestTracker
+    {
+        private List<FSEDataRequest> requests;
+        private int firstWindowRequestLimit = 10;
+        private int secondWindowRequestLimit = 40;
+        private string requestsFileName = "FSEDataRequests.txt";
+
+        public FSEDataRequestTracker()
+        {
+            requests = new List<FSEDataRequest>();
+
+            //read in all of the previosuly saved requests
+            LoadRequests();
+
+            //prune the list of requests and remove any that dont fall into one of the rolling windows
+            PruneRequests();
+        }
+
+        public void AddRequest(FSEDataRequest request)
+        {
+            requests.Add(request);
+        }
+
+        public List<FSEDataRequest> getRequests()
+        {
+            return requests;
+        }
+
+        
+        /// <summary>
+        /// Reuqests to FSE Data API are rate limited. check to see if we have exceeded that rate yet or if we can make another request.
+        /// </summary>
+        /// <returns></returns>
+        public bool CanMakeRequest()
+        {
+            //if we have less than 40 requests in the last 6 hours and less than 10 requests 
+            //in the last 60 seconds then we can make another requests           
+            if(RequestsInSecondWindow() < secondWindowRequestLimit && RequestsInFirstWindow() < firstWindowRequestLimit)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public TimeSpan TimeUntilNextRequest()
+        {
+            TimeSpan nextRequestTime = TimeSpan.Zero;
+
+            if (!CanMakeRequest())
+            {
+                //calculate how long we have to wait until we can make another request
+
+                //see if we hit the cap for the second window (40 requests in 6 hours)
+                if(RequestsInSecondWindow() == secondWindowRequestLimit)
+                {
+
+                }
+
+            }
+
+
+            return nextRequestTime;
+        }
+
+        /// <summary>
+        /// Every now and then we can prune requests from this list if they are outside of both of the rolling windows
+        /// </summary>
+        private void PruneRequests()
+        {
+            throw new NotImplementedException("PruneRequests");
+        }
+
+        /// <summary>
+        /// Load any existing requests from previous program executions.
+        /// </summary>
+        private void LoadRequests()
+        {
+            string pathToRequestsFile = Directory.GetCurrentDirectory() + "\\" + requestsFileName;
+            //See if the file exists
+            if (File.Exists(pathToRequestsFile))
+            {
+                string[] requestObjects = File.ReadAllLines(pathToRequestsFile);
+                //read line by line
+                foreach(string requestObjectStr in requestObjects)
+                {
+                    //parse into a request
+                    requests.Add(new FSEDataRequest(requestObjectStr));
+                }
+            }
+        }
+
+        public void SaveRequests()
+        {
+            throw new NotImplementedException("SaveRequests");
+        }
+
+        /// <summary>
+        /// This returns the number of requests that had occured during the first rollowing window.
+        /// The first rolling time window covers the past 10 minutes.
+        /// </summary>
+        /// <returns>The number of requests that have occured in the past 10 minutes.</returns>
+        private int RequestsInFirstWindow()
+        {
+            DateTime end = DateTime.Now;
+            DateTime start = end.Subtract(TimeSpan.FromMinutes(10));
+
+            int requestCount = 0;
+            if (requests.Count == 0)
+            {
+                return requestCount;
+            }
+            else
+            {
+                foreach (FSEDataRequest request in requests)
+                {
+                    //check to see if this request was fired off in the last 10 minutes
+                    if((request.GetTimestamp() > start ) && (request.GetTimestamp() < end)){
+                        requestCount++;
+                    }
+                }
+            }
+            return requestCount;
+        }
+
+        /// <summary>
+        /// This returns the number of requests that had occured during the second rollowing window.
+        /// The second rolling time window covers the past 24 hours.
+        /// </summary>
+        /// <returns>The number of requests that have occured in the past 10 minutes.</returns>
+        private int RequestsInSecondWindow()
+        {
+            DateTime end = DateTime.Now;
+            DateTime start = end.Subtract(TimeSpan.FromHours(6));
+
+            int requestCount = 0;
+            if (requests.Count == 0)
+            {
+                return requestCount;
+            }
+            else
+            {
+                foreach (FSEDataRequest request in requests)
+                {
+                    //check to see if this request was fired off in the last 6 hours
+                    if ((request.GetTimestamp() > start) && (request.GetTimestamp() < end))
+                    {
+                        requestCount++;
+                    }
+                }
+            }
+            return requestCount;
+        }
+    }
+}
