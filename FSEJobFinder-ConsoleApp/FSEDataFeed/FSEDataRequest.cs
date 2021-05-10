68c4462cadce14d89030bc8e2bf90a233e7109fc
treyturley <81778542+treyturley@users.noreply.github.com>
2021-05-09 23:26:26 -0500

    Implement equals comparison and refactor time window check

--- FSEJobFinder-ConsoleApp/FSEDataFeed/FSEDataRequest.cs
+++ FSEJobFinder-ConsoleApp/FSEDataFeed/FSEDataRequest.cs
@@ -3,7 +3,7 @@ using System.Net;
 
 namespace FSEDataFeed
 {
-    class FSEDataRequest
+    public class FSEDataRequest
     {
         private DateTime timeStamp;
         private string url;
@@ -65,6 +65,15 @@ namespace FSEDataFeed
         {
             return requestType;
         }
+        public bool isInTimeWindow(DateTime start, DateTime end)
+        {
+            //check to see if this request was fired off in the last 6 hours
+            if ((GetTimestamp() > start) && (GetTimestamp() < end))
+            {
+                return true;
+            }
+            return false;
+        }
 
         public override string ToString()
         {
@@ -75,6 +84,63 @@ namespace FSEDataFeed
             result += timeStamp.ToString();
 
             return result;
+        }
+
+        /// <summary>
+        /// override equals to see if this Data Request is the same of the passed in object.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>        
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            FSEDataRequest objAsDataRequest = obj as FSEDataRequest;
+            if(objAsDataRequest == null)
+            {
+                return false;
+            }
+            
+            return Equals(objAsDataRequest);
+        }
+
+        /// <summary>
+        /// Returns true if this Data Request is the same as the other Data Request
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(FSEDataRequest other)
+        {
+            if(this.requestType == other.requestType)
+            {
+                if (this.url.CompareTo(other.url) == 0)
+                {
+                    //when comparing date times, by default either the ticks (when using equals)
+                    //or the milliseconds when using CompareTo get compared. The datetime from the file doesnt have this info.
+
+                    //TODO: see if there is a better way to do this. 
+                    //manually compare each element
+                    if (this.timeStamp.Year.Equals(other.timeStamp.Year) &&
+                        this.timeStamp.Month.Equals(other.timeStamp.Month) &&
+                        this.timeStamp.Day.Equals(other.timeStamp.Day) &&
+                        this.timeStamp.Hour.Equals(other.timeStamp.Hour) &&
+                        this.timeStamp.Minute.Equals(other.timeStamp.Minute) &&
+                        this.timeStamp.Second.Equals(other.timeStamp.Second))
+                    {   
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return this.url.GetHashCode();
         }
 
     }
