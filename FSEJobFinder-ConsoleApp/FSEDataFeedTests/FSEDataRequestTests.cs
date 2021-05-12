de45049e6d6073bc8fdb59ae3bb14fee40b261fe
treyturley <81778542+treyturley@users.noreply.github.com>
2021-05-11 21:35:11 -0500

    Add constructor tests for FSEDataRequest

--- FSEJobFinder-ConsoleApp/FSEDataFeedTests/FSEDataRequestTests.cs
+++ FSEJobFinder-ConsoleApp/FSEDataFeedTests/FSEDataRequestTests.cs
@@ -6,10 +6,43 @@ namespace FSEDataFeed.Tests
     [TestClass]
     public class FSEDataRequestTests
     {
+        const string EMPTY_URL = "";
+        const string INVALID_URL = "http://localhost/TestMakeModelURL";
+        const string VALID_URL = @"https://server.fseconomy.net/data?userkey=TheUserKey&format=xml&query=aircraft&search=makemodel&makemodel=Cessna 172 Skyhawk";
+
+        
         [TestMethod]
-        public void requestTest()
+        [ExpectedException(typeof(UriFormatException), "An empty url was allowed.")]
+        public void Constructor_ThrowsUriFormatException()
         {
-            throw new NotImplementedException("Not Implemented");
+            FSEDataRequest request = new FSEDataRequest(FSEDataRequestType.Aircraft_By_MakeModel, EMPTY_URL);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception),"An invalid url was allowed.")]
+        public void Constructor_ThrowsException()
+        {   
+            FSEDataRequest request = new FSEDataRequest(FSEDataRequestType.Aircraft_By_MakeModel, INVALID_URL);
+        }
+
+        [TestMethod]
+        public void Constructor_Success()
+        {
+            FSEDataRequest request = new FSEDataRequest(FSEDataRequestType.Aircraft_By_MakeModel, VALID_URL);
+        }
+
+        /*
+        [TestMethod]
+        public void CacheTest()
+        {
+            //create a new request for Boieng 737 aircraft
+
+            //send the request and get the response
+
+            //checked the cache policy that was used
+
+            //send another request and see if the response is from the cache
+        }
+        */
     }
 }
